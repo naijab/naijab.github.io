0956e3dc245b89118c2584417d7822aa939ddc38
Nattapon Pondongnok <nainatjab999@gmail.com>
2020-08-28 10:41:32 +0700

    Updates

---
+++ _nuxt/static/1598586088/tag/web/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1598586088",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{},colorMode:{preference:b,value:b,unknown:true}}}("\u002Ftag\u002Fweb","system"));
