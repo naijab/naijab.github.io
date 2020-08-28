6855640c18f62d77569fc21383db1010eb736a1f
Nattapon Pondongnok <nainatjab999@gmail.com>
2020-08-28 10:18:16 +0700

    add router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  router: {
+    base: '/',
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
