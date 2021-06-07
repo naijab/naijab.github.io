ae4e5c85561e99ab71649a8ad5e3c7c70bb7950b
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-06-08 03:07:02 +0700

    add open link to blog

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,9 @@
       <h1 class="dark:text-white text-6xl">naijab.com</h1>
       <button
         class="bg-red-500 dark:bg-white dark:border-black dark:text-black text-xl text-white p-2 rounded"
-        @click="switchTheme"
+        @click="goToBlog"
       >
-        พบกันเร็วๆ นี้ - {{ theme }}
+        อ่าน Blog
       </button>
     </div>
   </div>
@@ -16,16 +16,40 @@
 
 <script>
 export default {
+  data() {
+    return {
+      countDown: 10,
+    }
+  },
+
   // TODO: Fetch Blog List from markdown
   computed: {
     theme() {
       return this.$colorMode.value === 'light' ? 'light' : 'dark'
     },
   },
+  created() {
+    this.countDownTimer()
+  },
   methods: {
     switchTheme() {
       this.$colorMode.preference =
         this.$colorMode.value === 'light' ? 'dark' : 'light'
+    },
+    goToBlog() {
+      this.location.href = 'https://naijab.medium.com/'
+    },
+    countDownTimer() {
+      if (this.countDown === 0) {
+        this.goToBlog()
+        return
+      }
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1
+          this.countDownTimer()
+        }, 1000)
+      }
     },
   },
 }
